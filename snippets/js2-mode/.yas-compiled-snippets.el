aa751b3e2ffe7a9eb9d48654ff0b9509737dc011
domtronn <dgc336@gmail.com>
2014-05-30 09:47:12 +0100

    Updating git ignore to ignore compiled yas snippets

--- snippets/js2-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `js2-mode'
-;;; Do not edit! File generated at Fri May 30 09:30:39 2014
--- snippets/lua-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `lua-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'lua-mode
-										 '(("fun" "function ()\n         ${1:return something}\nend" "fun" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Fri May 30 09:30:39 2014
