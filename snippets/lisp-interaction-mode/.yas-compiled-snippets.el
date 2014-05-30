abecc95f9b34c308316fbebc040d770c5a8ed918
Dom Charlesworth <dgc336@gmail.com>
2014-05-30 09:34:28 +0100

    Removed unused snippets

--- snippets/lisp-interaction-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `lisp-interaction-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'lisp-interaction-mode
-										 '(("defun" "(defun ${1:fun} (${2:args})\n       $0\n)" "defun" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Fri May 30 09:30:39 2014
