2831b6fa56c769dff8601fa69321a6708847ad40
domtronn <dgc336@gmail.com>
2014-07-25 08:48:02 +0100

    Added tabbar packages

---
+++ emacs.packages/elpa/tabbar-2.0.1/tabbar-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tabbar" "2.0.1" "Display a tab bar in the header line  -*-no-byte-compile: t; -*-" (quote nil))
---
+++ emacs.packages/elpa/tabbar-ruler-0.40/tabbar-ruler-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tabbar-ruler" "0.40" "Pretty tabbar, autohide, use both tabbar/ruler" (quote ((tabbar "2.0.1"))))
