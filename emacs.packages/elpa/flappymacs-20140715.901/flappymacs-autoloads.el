87d4223f305bf730e2ac9aada576fd34010fd5b1
Dom Charlesworth <dgc336@gmail.com>
2014-08-05 16:35:29 +0100

    Added Flappymacs package game

---
+++ emacs.packages/elpa/flappymacs-20140715.901/flappymacs-autoloads.el
@@ -0,0 +1,33 @@
+;;; flappymacs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flappymacs) "flappymacs" "flappymacs.el" (21472
+;;;;;;  53604 0 0))
+;;; Generated autoloads from flappymacs.el
+
+(autoload 'flappymacs "flappymacs" "\
+Play flappybird and waste time.
+
+flappymacs-mode keybindings:\\<flappymacs-mode-map>
+
+\\{flappymacs-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flappymacs-pkg.el") (21472 53604 780932
+;;;;;;  0))
+
+;;;***
+
+(provide 'flappymacs-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flappymacs-autoloads.el ends here
---
+++ emacs.packages/elpa/flappymacs-20140715.901/flappymacs-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flappymacs" "20140715.901" "flappybird clone for emacs" (quote nil))
