c9f921c72e1d29c1cebbbbc1c524b5d1ac418720
Dom Charlesworth <dgc336@gmail.com>
2014-08-07 09:40:20 +0100

    Adding ibuffer packages -git (show git status in ibuffer) -vc  (show vc status in ibuffer)

---
+++ emacs.packages/elpa/ibuffer-git-20110508.31/ibuffer-git-autoloads.el
@@ -0,0 +1,18 @@
+;;; ibuffer-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ibuffer-git-pkg.el" "ibuffer-git.el")
+;;;;;;  (21475 13062 489475 0))
+
+;;;***
+
+(provide 'ibuffer-git-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ibuffer-git-autoloads.el ends here
