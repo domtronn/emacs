bccc8d4da5414bfafad902cbb0e4cf24db7cb6d3
Dom Charlesworth <dgc336@gmail.com>
2014-08-07 09:39:34 +0100

    Added Dired Packages -rainbow (colourful dired mode) -subtree (allows to drill into same directory) -k			 (allows for higlight with git status) -filter  (ibuffer like filtering)

---
+++ emacs.packages/elpa/dired-k-20140701.347/dired-k-autoloads.el
@@ -0,0 +1,41 @@
+;;; dired-k-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (dired-k) "dired-k" "dired-k.el" (21475 14648 0
+;;;;;;  0))
+;;; Generated autoloads from dired-k.el
+
+(autoload 'dired-k "dired-k" "\
+Highlighting dired buffer by file size, last modified time, and git status.
+This is inspired by `k' zsh script
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (direx-k) "direx-k" "direx-k.el" (21475 14648 0
+;;;;;;  0))
+;;; Generated autoloads from direx-k.el
+
+(autoload 'direx-k "direx-k" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("dired-k-pkg.el") (21475 14648 596177
+;;;;;;  0))
+
+;;;***
+
+(provide 'dired-k-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dired-k-autoloads.el ends here
