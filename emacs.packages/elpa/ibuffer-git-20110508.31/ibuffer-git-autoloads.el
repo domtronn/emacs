96552bdc23472e17e4d10ca68c16b9b6820b6a0e
Dom Charlesworth <dgc336@gmail.com>
2014-08-11 13:43:25 +0100

    Removed ibuffer-git

--- emacs.packages/elpa/ibuffer-git-20110508.31/ibuffer-git-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; ibuffer-git-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("ibuffer-git-pkg.el" "ibuffer-git.el")
-;;;;;;  (21475 13062 489475 0))
-
-;;;***
-
-(provide 'ibuffer-git-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ibuffer-git-autoloads.el ends here
