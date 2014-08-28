cf54a41117bb5e36a67fa18449241a109bd615b8
Dom Charlesworth <dgc336@gmail.com>
2014-08-28 08:42:56 +0100

    Reformatted

--- elisp/emms-get-lyrics.el
+++ elisp/emms-get-lyrics.el
@@ -1,21 +1,21 @@
 ;;; emms-get-lyrics.el --- Get the lyrics of the song emms is currently playing
 
-;; Copyright (C) 2007 Jay Belanger
-;; Copyright (C) 2010 Marcos Talau (talau@users.sourceforge.net)
+;; Copyright(C) 2007 Jay Belanger
+;; Copyright(C) 2010 Marcos Talau(talau@users.sourceforge.net)
 
-;; emms-get-lyrics.el is free software; you can redistribute it and/or 
-;; modify it under the terms of the GNU General Public License as 
-;; published by the Free Software Foundation; either version 2, or 
+;; emms-get-lyrics.el is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
 ;; (at your option) any later version.
 
-;; emms-get-lyrics.el is distributed in the hope that it will be 
-;; useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+;; emms-get-lyrics.el is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 ;; of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; General Public License for more details.
 
 ;;; Commentary:
 
-;; The function 'emms-get-lyrics-current-song' tries to get the lyrics
+;; The function "emms-get-lyrics-current-song" tries to get the lyrics
 ;; to the song that emms is currently playing.  It currently requires
 ;; w3m to get the lyrics.  It copies the lyrics to a file ending in
 ;; .lyrics; if the variable `emms-get-lyrics-use-files' is nil, it
@@ -33,18 +33,18 @@
 ;;; User Customization
 
 (defvar emms-get-lyrics-use-files t)
-(defvar emms-get-lyrics-dir nil)
+(defvar emms-get-lyrics-dir "~/Music/Lyrics")
 (defvar emms-get-lyrics-match-lyric-notfound "This page needs content.")
 (defvar emms-get-lyrics-match-ad-top-ringtone "Ringtone to your\\(.*?\\)Ad")
 (defvar emms-get-lyrics-match-ad-top-javascript "You must enable javascript\\(.*?\\)\n\\(.*?\\)Gracenote.")
 (defvar emms-get-lyrics-match-ad-bottom-ringtone "Ringtone to your")
 (defvar emms-get-lyrics-debug nil)
 
-(defun emms-get-lyrics-url (artist title)
+(defun emms-get-lyrics-url(artist title)
   "Return the URL of lyric."
-  (let ((url (concat
+  (let((url(concat
    "http://www.lyricwiki.org/index.php?title="
-   (replace-regexp-in-string 
+   (replace-regexp-in-string
     " " "_"
     (concat
      artist
@@ -53,50 +53,50 @@
    "&printable=yes")))
     url))
 
-(defun emms-get-lyrics-w3m (url buffer)
+(defun emms-get-lyrics-w3m(url buffer)
   "Get the lyric in the Internet and put it in the BUFFER."
   (if emms-get-lyrics-debug
       (message "calling: w3m -dump %s" url))
   (call-process "w3m" nil buffer nil "-dump" url))
 
-(defun emms-get-lyrics-redirect-check (url buffer)
+(defun emms-get-lyrics-redirect-check(url buffer)
   "If need to redirect to another site, do this!"
-  (goto-char (point-min))
-  (cond ((search-forward "#REDIRECT" nil t)
+  (goto-char(point-min))
+  (cond((search-forward "#REDIRECT" nil t)
 	 (if emms-get-lyrics-debug
 	     (message "Redirecting to %s" url))
-	 (delete-region (point-min) (point-max))
+	 (delete-region(point-min) (point-max))
 	 (emms-get-lyrics-w3m url buffer))))
 
-(defun emms-get-lyrics-write-lyric (bname title artist)
+(defun emms-get-lyrics-write-lyric(bname title artist)
   "When Lyric is not found, ask the user if he wants to write it."
   (switch-to-buffer bname)
-  (delete-region (point-min) (point-max))
+  (delete-region(point-min) (point-max))
   (insert ";; This lyric is not found at LyricWiki.org\n")
   (insert ";; You can write the lyric or try to find it\n")
   (insert ";; in the Internet.\n")
   (insert ";; Once you writted or finded, please, post it in LyricWiki.org\n\n")
   (insert title " (" artist ")\n\n"))
 
-(defun emms-get-lyrics (artist title fn &optional file)
+(defun emms-get-lyrics(artist title fn &optional file)
   "Core function. Get lyrics, put it in the buffer and format it."
-  (let ((bname (concat "Lyrics: " title " (" artist ")")))
-    (cond ((get-buffer bname)
+  (let((bname(concat "Lyrics: " title " (" artist ")")))
+    (cond((get-buffer bname)
            (switch-to-buffer bname))
-          ((and file (file-exists-p file))
+          ((and file(file-exists-p file))
            (find-file file)
            (rename-buffer bname))
           (t
-           (let ((buffer (if file
+           (let((buffer(if file
                              (find-file-noselect file)
                            (get-buffer-create bname))))
              (set-buffer buffer)
-             (funcall fn (emms-get-lyrics-url artist title) buffer)
-             (goto-char (point-min))
-             (if (not (search-forward 
+             (funcall fn(emms-get-lyrics-url artist title) buffer)
+             (goto-char(point-min))
+             (if (not(search-forward
                         emms-get-lyrics-match-lyric-notfound nil t))
 		 ; where lyrics from?
-                 (let ((frominsert
+                 (let((frominsert
                         (save-excursion
                           (if (re-search-forward "^Retrieved from")
 			    (buffer-substring-no-properties
@@ -104,21 +104,21 @@
                                (- (line-end-position) 1))
                             "From LyricWiki"))))
 		   (emms-get-lyrics-redirect-check frominsert buffer)
-		   (goto-char (point-min))
+		   (goto-char(point-min))
                    (insert "(from " frominsert ")\n\n")
                    (insert title " (" artist ")\n")
 		   ; delete ads, and save disk space ;D
-			 (let ((trash-point (point)))
+			 (let((trash-point(point)))
 				 (if (or
-							 (search-forward-regexp emms-get-lyrics-match-ad-top-ringtone (point-max) t)
-							 (search-forward-regexp emms-get-lyrics-match-ad-top-javascript (point-max) t)
+							 (search-forward-regexp emms-get-lyrics-match-ad-top-ringtone(point-max) t)
+							 (search-forward-regexp emms-get-lyrics-match-ad-top-javascript(point-max) t)
 							)
-		     (delete-region trash-point (point))))
-		   (search-forward emms-get-lyrics-match-ad-bottom-ringtone (point-max) t)
+		     (delete-region trash-point(point))))
+		   (search-forward emms-get-lyrics-match-ad-bottom-ringtone(point-max) t)
 		   (move-beginning-of-line nil)
 		   (kill-line)
 		   ; delete bottom
-                   (goto-char (point-max))   (move-beginning-of-line nil)
+                   (goto-char(point-max))   (move-beginning-of-line nil)
                    (if (or
 												(search-backward "Credits" (point-min) t)
 												(search-backward "Written by" (point-min) t)
@@ -127,36 +127,36 @@
                         (search-backward "External links" (point-min) t)
                         (search-backward "Retrieved from" (point-min) t)
 												(search-backward "ITunes icon iTunes" (point-min) t))
-                       (delete-region (point) (point-max)))
-                   (when file 
+                       (delete-region(point) (point-max)))
+                   (when file
                      (rename-buffer bname)
                      (save-buffer))
-		   (goto-char (point-min))
+		   (goto-char(point-min))
 		   (switch-to-buffer buffer)
-		   (goto-char (point-min)))
+		   (goto-char(point-min)))
 	       (restore-buffer-modified-p nil)
-	       (if (y-or-n-p (concat "Sorry, unable to find lyrics for " artist " (" file "). Do you want help writing this lyrics now? (say yes :-) ) "))
+	       (if (y-or-n-p(concat "Sorry, unable to find lyrics for " artist " (" file "). Do you want help writing this lyrics now? (say yes :-) ) "))
 		   (emms-get-lyrics-write-lyric bname title artist)
 		 (kill-buffer))
                ))))))
-  
-(defun emms-get-lyrics-current-song ()
+
+(defun emms-get-lyrics-current-song()
   "From current song extract info and find the Lyric."
   (interactive)
-  (let* ((track (emms-playlist-current-selected-track))
-         (artist (cdr (assoc 'info-artist track)))
-         (title (cdr (assoc 'info-title track))))
-    (cond ((not emms-player-playing-p)
+  (let* ((track(emms-playlist-current-selected-track))
+         (artist(cdr(assoc 'info-artist track)))
+         (title(cdr(assoc 'info-title track))))
+    (cond((not emms-player-playing-p)
 	   (message "Nothing playing right now"))
-	  ((not (executable-find "w3m"))
+	  ((not(executable-find "w3m"))
 	   (message "You need the program w3m"))
-	  ((not (executable-find emms-info-mp3info-program-name))
-	   (message "You need the program %s (If you install it, reopen your Emacs.)" emms-info-mp3info-program-name))
+	  ((not(executable-find emms-info-mp3info-program-name))
+	   (message "You need the program %s(If you install it, reopen your Emacs.)" emms-info-mp3info-program-name))
 	  ((not artist)
 	   (message "No artist name in current song"))
 	  ((not title)
 	   (message "No title in current song"))
-	  (t (emms-get-lyrics artist title 'emms-get-lyrics-w3m
+	  (t(emms-get-lyrics artist title 'emms-get-lyrics-w3m
                          (if emms-get-lyrics-use-files
                              (if emms-get-lyrics-dir
                                  (concat
@@ -170,7 +170,7 @@
                                     title
                                     ".lyrics")))
                                (concat
-                                (file-name-sans-extension (cdr (assoc 'name track)))
+                                (file-name-sans-extension(cdr(assoc 'name track)))
                                 ".lyrics"))))))))
 
 (provide 'emms-get-lyrics)
