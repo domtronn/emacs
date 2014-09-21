e8dd9ec09ddcf78ede55eb32b04db8f525009e56
Dom Charlesworth <dgc336@gmail.com>
2014-09-21 16:27:22 +0100

    Added function to swap two regions
    This allows for the swapping of expects and actuals method in jasmine
    tests when they've been placed the wrong way round

--- functions.el
+++ functions.el
@@ -1247,6 +1247,50 @@ otherwise raises an error."
   (end-of-line)
   (insert (format "%s" ";")))
 
+(defun swap-regions (beg1 end1 beg2 end2)
+  "Swap region between BEG1 and END1 with region BEG2 and END2.
+
+For the first region, mark the first region and set mark at
+point.  The second region only needs to be marked normally.
+Again, set the mark at the beginning and end of the first region,
+then mark the second region with mark and point.
+
+The order of the two regions in the buffer doesn't matter.
+Either one can precede the other.  However, the regions can not
+be swapped if they overlap.
+
+All arguments can either be a number for a position in the buffer
+or a marker."
+  (interactive
+   (if (< (length mark-ring) 2)
+       (error "Not enough in mark-ring to swap a region")
+     (let ((region (list (region-beginning) (region-end)))
+	   (marks (sort (list (marker-position (car mark-ring))
+			      (marker-position (cadr mark-ring)))
+			'<)))
+       (if (< (car region) (car marks))
+	   (append region marks)
+	 (append marks region)))))
+  (if (or (and (< beg2 beg1) (< beg1 end2))
+	  (and (< beg1 beg2) (< beg2 end1)))
+      (error "Unable to swap overlapping regions")
+      (save-excursion
+	(insert 
+	 (prog1 (delete-and-extract-region beg2 end2)
+	   (goto-char beg2)
+	   (insert 
+	    (delete-and-extract-region beg1 end1))
+	   (goto-char beg1))))))
+
+(defun swap-expect-and-actual ()
+  (interactive)
+	(save-excursion
+		(let ((p1 (search-forward "("))
+					(p2 (- (search-forward ")") 1))
+					(p3 (search-forward "("))
+					(p4 (- (search-forward ");") 2)))
+			(swap-regions p1 p2 p3 p4))))
+
 (defun convert-css (from to)
   (interactive "nConvert from resolution : \nnTo resolution : ")
 	(let ((factor (/ (float to) (float from))))
