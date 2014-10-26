15a15772967b5de7284c0a34d8fc33dfa0d76a99
Dom Charlesworth <dgc336@gmail.com>
2014-10-26 18:41:51 +0000

    Added python script to create file alist
    Wrote a python script to create the alist needed for filecacheing.
    This was done to optimise the file cache functionality, the python script runs faster than file-cache-... functions

---
+++ create-file-alist.py
@@ -0,0 +1,21 @@
+import os, re, sys, json
+
+project_dir = sys.argv[1]
+filter_regexp = sys.argv[2].split(",")
+
+resultant_files = []
+for root, dirs, files in os.walk(project_dir):
+    for name in files:
+        resultant_files.append(os.path.join(root, name))
+
+for r in filter_regexp:
+    resultant_files = [f for f in resultant_files if not re.compile(r).search(f)]
+        
+result_dict = {}
+for f in resultant_files:
+    if os.path.basename(f) in result_dict:
+        result_dict[os.path.basename(f)].append(os.path.dirname(f))
+    else:
+        result_dict[os.path.basename(f)] = [os.path.dirname(f)]
+
+print json.dumps(result_dict)
