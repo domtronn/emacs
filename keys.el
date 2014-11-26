bd7b7c6b96ff5a2950b7dec939c409a7f9b376a7
Dom Charlesworth <dgc336@gmail.com>
2014-11-26 13:11:37 +0000

    Override M-x to use smex

--- keys.el
+++ keys.el
@@ -40,6 +40,11 @@
 (global-set-key (kbd "s-W") 
   '(lambda () (interactive) (kill-buffer (buffer-name))))
 
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 ;; Undo and Redo
 (global-set-key (kbd "s-z") 'undo-tree-undo)
 (global-set-key (kbd "s-Z") 'undo-tree-redo)
