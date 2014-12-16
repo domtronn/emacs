925a0cf53f3977d2c295bbc84ff780b23ad428a5
Dom Charlesworth <dgc336@gmail.com>
2014-12-16 12:44:03 +0000

    Added Themes

---
+++ emacs.packages/elpa/gruvbox-theme-20141128.816/gruvbox-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gruvbox-theme" "20141128.816" "A retro-groove colour theme for Emacs" 'nil :url "http://github.com/Greduan/emacs-theme-gruvbox")
---
+++ emacs.packages/elpa/monokai-theme-20141121.1202/monokai-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "monokai-theme" "20141121.1202" "A fruity color theme for Emacs." 'nil :url "http://github.com/oneKelvinSmith/monokai-emacs")
