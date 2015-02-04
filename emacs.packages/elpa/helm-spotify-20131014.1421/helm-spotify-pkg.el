5fe0a11e32d6ad6bfd6ed23f2ebc910b63b418b0
Dom Charlesworth <dgc336@gmail.com>
2015-02-04 13:45:58 +0000

    Adding helm & helm spotify

---
+++ emacs.packages/elpa/helm-spotify-20131014.1421/helm-spotify-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-spotify" "20131014.1421" "Control Spotify with Helm." '((helm "0.0.0") (multi "2.0.0")) :url "https://github.com/krisajenkins/helm-spotify" :keywords '("helm" "spotify"))
