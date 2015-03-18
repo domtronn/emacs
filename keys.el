c926f55a3ddf80ebe43227f2a4ae151dac986387
Dom Charlesworth <dgc336@gmail.com>
2015-03-18 13:52:09 +0000

    Remove key bindings for swiper

--- keys.el
+++ keys.el
@@ -82,7 +82,11 @@
 (global-set-key (kbd "C-x f") 'ido-find-file)
 
 ;; change to use regexp searching rather than normal isearch
-(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "H-s") 'isearch-forward-symbol-at-point)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-?") 'query-replace-regexp)
+
 
 (global-set-key (kbd "M-?") 'ispell-word)
 
