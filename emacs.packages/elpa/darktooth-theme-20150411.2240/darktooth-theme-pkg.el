e2a39ca0cbcf09d5f575ad031b0148cfa9ecd648
Dom Charlesworth <dgc336@gmail.com>
2015-04-14 11:57:09 +0100

    Update of packages

---
+++ emacs.packages/elpa/darktooth-theme-20150411.2240/darktooth-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darktooth-theme" "20150411.2240" "colour theme for Emacs, a remix of gruvbox" 'nil :url "http://github.com/emacsfodder/emacs-theme-darktooth")
---
+++ emacs.packages/elpa/expand-region-20150413.1435/expand-region-pkg.el
@@ -0,0 +1,4 @@
+(define-package "expand-region" "20150413.1435" "Increase selected region by semantic units." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
