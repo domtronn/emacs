46dba0884e0a024241c5f117fa2f7cef6b2c47e3
Dom Charlesworth <dgc336@gmail.com>
2015-05-08 13:57:57 +0100

    Remove centralised powerline

--- elisp/powerline.el
+++ elisp/powerline.el
@@ -513,7 +513,6 @@ install the memoized function over the original function."
                              (powerline-major-mode     'left        powerline-color1  )
                              (powerline-process        'text        powerline-color1  )
                              (powerline-narrow         'left        powerline-color1  powerline-color2  )
-                             (powerline-global         'center                        powerline-color2  )
                              (powerline-vc             'center                        powerline-color2  )
                              (powerline-make-fill                                     powerline-color2  )
                              (powerline-row            'right       powerline-color1  powerline-color2  )
