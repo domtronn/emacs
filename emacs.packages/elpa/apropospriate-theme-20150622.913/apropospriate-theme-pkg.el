4683730f699c6653eb97d9e5698c289153e324fe
Dom Charlesworth <dgc336@gmail.com>
2015-07-03 09:40:39 +0100

    Remove appropriate theme

--- emacs.packages/elpa/apropospriate-theme-20150622.913/apropospriate-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "apropospriate-theme" "20150622.913" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
