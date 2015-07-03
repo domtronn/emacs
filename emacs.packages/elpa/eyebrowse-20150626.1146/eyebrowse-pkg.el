9d84ebadd7b59e250ac4aac944509969458721a0
Dom Charlesworth <dgc336@gmail.com>
2015-07-03 09:09:34 +0100

    Added eyebrowse package

---
+++ emacs.packages/elpa/eyebrowse-20150626.1146/eyebrowse-pkg.el
@@ -0,0 +1,1 @@
+(define-package "eyebrowse" "20150626.1146" "Easy window config switching" '((dash "2.7.0") (emacs "24.3.1")) :url "https://github.com/wasamasa/eyebrowse" :keywords '("convenience"))
