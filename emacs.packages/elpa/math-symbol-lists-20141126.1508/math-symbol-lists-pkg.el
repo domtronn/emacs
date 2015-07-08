8aed57102088815feddc222a28df5a30cdda83ae
Dom Charlesworth <dgc336@gmail.com>
2015-07-08 09:09:03 +0100

    Added math symbol lists used by tex mode

---
+++ emacs.packages/elpa/math-symbol-lists-20141126.1508/math-symbol-lists-pkg.el
@@ -0,0 +1,1 @@
+(define-package "math-symbol-lists" "20141126.1508" "Lists of Unicode mathematical symbols and latex commands" 'nil :url "https://github.com/vspinu/math-symbol-lists" :keywords '("unicode" "symbols" "mathematics"))
