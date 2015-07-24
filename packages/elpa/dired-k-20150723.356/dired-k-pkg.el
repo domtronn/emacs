1e6a14663c46e38ead8251874d2ab5c20e1754a7
Dom Charlesworth <dgc336@gmail.com>
2015-07-24 07:23:46 +0100

    Updated packages

---
+++ packages/elpa/dired-k-20150723.356/dired-k-pkg.el
@@ -0,0 +1,7 @@
+(define-package "dired-k" "20150723.356" "highlight dired buffer by file size, modified time, git status"
+  '((cl-lib "0.5")
+    (emacs "24"))
+  :url "https://github.com/syohex/emacs-dired-k")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
