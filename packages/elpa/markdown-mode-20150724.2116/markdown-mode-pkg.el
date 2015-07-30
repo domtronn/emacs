a5b9a04f86c79bc66c53768690067c47bd8ac381
Dom Charlesworth <dgc336@gmail.com>
2015-07-30 08:32:07 +0100

    Updated packages

--- packages/elpa/markdown-mode-20150724.2116/markdown-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "markdown-mode" "20150724.2116" "Emacs Major mode for Markdown-formatted text files" 'nil :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
---
+++ packages/elpa/shell-pop-20150727.2314/shell-pop-pkg.el
@@ -0,0 +1,1 @@
+(define-package "shell-pop" "20150727.2314" "helps you to use shell easily on Emacs. Only one key action to work." '((emacs "24")) :url "http://github.com/kyagi/shell-pop-el" :keywords '("shell" "terminal" "tools"))
