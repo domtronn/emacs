47f1ff11454bee4d3c8149163a47ec4f862e9a4b
Dom Charlesworth <dgc336@gmail.com>
2015-08-05 14:57:33 +0100

    Fix web mode matching

--- init.el
+++ init.el
@@ -237,14 +237,14 @@
 				))
 
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.partial\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.partial" . web-mode))
 
 (define-key web-mode-map (kbd "s-/") 'web-mode-comment-or-uncomment)
 
