56948303f37b78ad732d8aca9fc19492c24a9319
Dom Charlesworth <dgc336@gmail.com>
2015-08-10 09:41:50 +0100

    Updated packages

--- packages/elpa/ujelly-theme-20150705.303/ujelly-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ujelly-theme" "20150705.303" "Ujelly theme for GNU Emacs 24 (deftheme)" 'nil :url "http://github.com/marktran/color-theme-ujelly")
---
+++ packages/elpa/ujelly-theme-20150807.2136/ujelly-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ujelly-theme" "20150807.2136" "Ujelly theme for GNU Emacs 24 (deftheme)" 'nil :url "http://github.com/marktran/color-theme-ujelly")
