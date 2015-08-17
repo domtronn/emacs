7b616010d86ee457a8c014f6b29d372515b37652
Dom Charlesworth <dgc336@gmail.com>
2015-08-17 09:31:38 +0100

    Update packages

--- packages/elpa/grunt-20150805.718/grunt-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "grunt" "20150805.718" "Some glue to stick Emacs and Gruntfiles together" '((dash "2.9.0")) :url "https://github.com/gempesaw/grunt.el" :keywords '("convenience" "grunt"))
