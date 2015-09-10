bb344780349c2f9b68b40e3622d9540c11568a09
Dom Charlesworth <dgc336@gmail.com>
2015-09-10 10:59:00 +0100

    Added functions which scale font size globally

--- functions.el
+++ functions.el
@@ -854,5 +854,20 @@ or a marker."
   (flycheck-mode 0)
   (font-lock-mode 0))
 
+(defun font-scale-increase ()
+  "Increase the font height globablly."
+  (interactive)
+  (font-scale '+ 10))
+
+(defun font-scale-decrease ()
+  "Increase the font height globablly."
+  (interactive)
+  (font-scale '- 10))
+
+(defun font-scale (op &optional amount)
+  "Apply function operator OP (+/-) to scale face attribute height by AMOUNT."
+  (let ((height (face-attribute 'default :height)))
+    (set-face-attribute 'default nil :height (funcall op height (or amount 10)))))
+
 (provide 'functions)
 ;;; functions.el ends here
