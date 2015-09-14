f31a4a88186c4c45a134875b383f63d339030ccc
Dom Charlesworth <dgc336@gmail.com>
2015-09-14 08:34:48 +0100

    Removed some themes

--- packages/elpa/farmhouse-theme-20150506.2127/farmhouse-theme-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; farmhouse-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "farmhouse-dark-theme" "farmhouse-dark-theme.el"
-;;;;;;  (21836 52666 0 0))
-;;; Generated autoloads from farmhouse-dark-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "farmhouse-light-theme" "farmhouse-light-theme.el"
-;;;;;;  (21836 52666 0 0))
-;;; Generated autoloads from farmhouse-light-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "farmhouse-theme-common" "farmhouse-theme-common.el"
-;;;;;;  (21836 52666 0 0))
-;;; Generated autoloads from farmhouse-theme-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("farmhouse-theme-pkg.el") (21836 52666
-;;;;;;  726600 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; farmhouse-theme-autoloads.el ends here
