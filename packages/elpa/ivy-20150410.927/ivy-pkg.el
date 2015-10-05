2509a55b10efbcc718ee8ded87591e510f5174c2
Dom Charlesworth <dgc336@gmail.com>
2015-10-05 10:59:02 +0100

    Updated swiper

--- packages/elpa/ivy-20150410.927/ivy-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ivy" "20150410.927" "Incremental Vertical completYon" '((emacs "24.1")) :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
--- packages/elpa/swiper-20151004.957/swiper-autoloads.el
+++ packages/elpa/swiper-20151004.957/swiper-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ivy" "ivy.el" (22034 11967 0 0))
+;;;### (autoloads nil "ivy" "ivy.el" (22034 18871 0 0))
 ;;; Generated autoloads from ivy.el
 
 (autoload 'ivy-resume "ivy" "\
@@ -65,7 +65,7 @@ Find a file on `recentf-list'.
 
 ;;;***
 
-;;;### (autoloads nil "swiper" "swiper.el" (22034 11966 0 0))
+;;;### (autoloads nil "swiper" "swiper.el" (22034 18871 0 0))
 ;;; Generated autoloads from swiper.el
 
 (autoload 'swiper-avy "swiper" "\
@@ -82,7 +82,7 @@ When non-nil, INITIAL-INPUT is the initial search pattern.
 ;;;***
 
 ;;;### (autoloads nil nil ("colir.el" "ivy-hydra.el" "swiper-pkg.el")
-;;;;;;  (22034 11967 95594 0))
+;;;;;;  (22034 18871 210392 0))
 
 ;;;***
 
