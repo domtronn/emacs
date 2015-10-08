3ca5c7a97d07185b8c5b1dfe2874b40840e88fa9
Dom Charlesworth <dgc336@gmail.com>
2015-10-08 09:13:50 +0100

    Deleted old packages

--- packages/elpa/dash-20151004.257/dash-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dash" "20151004.257" "A modern list library for Emacs" 'nil :keywords '("lists"))
--- packages/elpa/web-mode-20151002.339/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20151002.339" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
