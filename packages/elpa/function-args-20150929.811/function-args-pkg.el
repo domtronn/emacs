e618dcd444bcafda4acf969bf4b400ea54606665
Dom Charlesworth <dgc336@gmail.com>
2015-10-08 21:41:00 +0100

    Removed swiper

--- packages/elpa/function-args-20150929.811/function-args-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "function-args" "20150929.811" "C++ completion for GNU Emacs"
-  '((swiper "0.2.0"))
-  :url "https://github.com/abo-abo/function-args")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
