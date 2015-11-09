fb85481f6a1720ff5eb9b9eb2fb4a05bb4920a1b
Dom Charlesworth <dgc336@gmail.com>
2015-11-09 09:49:09 +0000

    Removed old unused packages

--- packages/elpa/dom-20150414.1610/dom-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dom" "20150414.1610" "DOM implementation and manipulation library" '((cl-lib "0.5")) :url "http://www.github.com/toroidal-code/dom.el/" :keywords '("xml" "dom"))
--- packages/elpa/show-css-20140816.1008/show-css-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; show-css-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "show-css" "show-css.el" (21895 47285 0 0))
-;;; Generated autoloads from show-css.el
-
-(autoload 'showcss-mode "show-css" "\
-Display the css of the class or id the cursor is at.
-Visit https://github.com/smmcg/showcss-mode to view the
-git repository
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("buffer-combine.el" "show-css-pkg.el")
-;;;;;;  (21895 47285 649519 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; show-css-autoloads.el ends here
