5f937857fa44132352ac65bc91a7eb58fca345b7
Dom Charlesworth <dgc336@gmail.com>
2016-03-08 09:15:50 +0000

    Updated packages

---
+++ packages/elpa/arjen-grey-theme-20160307.1304/arjen-grey-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; arjen-grey-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "arjen-grey-theme" "arjen-grey-theme.el" (22238
+;;;;;;  38891 0 0))
+;;; Generated autoloads from arjen-grey-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("arjen-grey-theme-pkg.el") (22238 38891
+;;;;;;  139616 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; arjen-grey-theme-autoloads.el ends here
--- packages/elpa/helm-core-20160305.2327/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20160305.2327" "Development files for Helm"
-  '((emacs "24.3")
-    (async "1.6"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
