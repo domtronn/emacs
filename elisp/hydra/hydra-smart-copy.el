ce4ddc8fbf4573d08c0d64b8cc3d32faaa6cdbcc
Dom Charlesworth <dgc336@gmail.com>
2016-05-30 22:44:37 +0100

    Swap around smart copy inside/outside bindings

--- elisp/hydra/hydra-smart-copy.el
+++ elisp/hydra/hydra-smart-copy.el
@@ -11,18 +11,18 @@
   "
 ^Mark^                    | ^Files^        | ^Pairs^            | ^Quotes^
 ------------------------+--------------^+^------------------^+^-----------------------
-_w_: word    _._: sentence  | _d_: file dir  | _P_: Inside Pairs  | _O_: Inside Quotes
-_s_: symbol  _h_: paragraph | _f_: file name | _p_: Outside Pairs | _o_: Outside Quotes
+_w_: word    _._: sentence  | _d_: file dir  | _p_: Inside Pairs  | _o_: Inside Quotes
+_s_: symbol  _h_: paragraph | _f_: file name | _P_: Outside Pairs | _O_: Outside Quotes
 _d_: defun   _u_: url       | _F_: full path
                         | _b_: buffer
 "
   ("w" (hydra/smart-copy 'er/mark-word) :exit t :color teal)
   ("s" (hydra/smart-copy 'er/mark-symbol) :exit t :color teal)
   ("d" (hydra/smart-copy 'er/mark-defun) :exit t :color teal)
-  ("P" (hydra/smart-copy 'er/mark-inside-pairs) :exit t :color teal)
-  ("p" (hydra/smart-copy 'er/mark-outside-pairs) :exit t :color teal)
-  ("O" (hydra/smart-copy 'er/mark-inside-quotes) :exit t :color teal)
-  ("o" (hydra/smart-copy 'er/mark-outside-quotes) :exit t :color teal)
+  ("p" (hydra/smart-copy 'er/mark-inside-pairs) :exit t :color teal)
+  ("P" (hydra/smart-copy 'er/mark-outside-pairs) :exit t :color teal)
+  ("o" (hydra/smart-copy 'er/mark-inside-quotes) :exit t :color teal)
+  ("O" (hydra/smart-copy 'er/mark-outside-quotes) :exit t :color teal)
   ("." (hydra/smart-copy 'er/mark-text-sentence) :exit t :color teal)
   ("h" (hydra/smart-copy 'er/mark-text-paragraph) :exit t :color teal)
   ("b" (kill-new (buffer-name)) :exit t :color teal)
