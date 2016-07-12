295520e7b2ab26c0ac631a7f1ead925a7f9febb6
Dom Charlesworth <dgc336@gmail.com>
2016-07-12 20:00:17 +0100

    Update packages

---
+++ packages/elpa/flycheck-tip-20160622.1906/flycheck-tip-pkg.el
@@ -0,0 +1,9 @@
+(define-package "flycheck-tip" "20160622.1906" "Show flycheck/flymake errors by tooltip"
+  '((flycheck "0.13")
+    (emacs "24.1")
+    (popup "0.5.0"))
+  :url "https://github.com/yuutayamada/flycheck-tip" :keywords
+  '("flycheck"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ packages/elpa/leuven-theme-20160705.431/leuven-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "leuven-theme" "20160705.431" "Awesome Emacs color theme on white background" 'nil :url "https://github.com/fniessen/emacs-leuven-theme" :keywords '("color" "theme"))
