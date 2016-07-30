e0c22464a2fe629e37ac291965f61f59c2f32116
Dom Charlesworth <dgc336@gmail.com>
2016-07-30 15:24:13 +0100

    Update packages

---
+++ packages/elpa/marshal-20160725.1851/marshal-pkg.el
@@ -0,0 +1,1 @@
+(define-package "marshal" "20160725.1851" "eieio extension for automatic (un)marshalling" '((eieio "1.4") (json "1.3")) :url "https://github.com/sigma/marshal.el" :keywords '("eieio"))
---
+++ packages/elpa/ox-reveal-20160719.28/ox-reveal-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ox-reveal" "20160719.28" "reveal.js Presentation Back-End for Org Export Engine" '((org "20150330")) :keywords '("outlines" "hypermedia" "slideshow" "presentation"))
