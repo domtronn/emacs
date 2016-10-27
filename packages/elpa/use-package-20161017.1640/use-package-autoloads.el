685b66cb73615714367e6c115840fc2093deea36
Dom Charlesworth <dgc336@gmail.com>
2016-10-27 16:19:18 +0100

    Update `use-package` package

--- packages/elpa/use-package-20161017.1640/use-package-autoloads.el
+++ packages/elpa/use-package-20161017.1640/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22496 61402
+;;;### (autoloads nil "use-package" "use-package.el" (22546 6696
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
