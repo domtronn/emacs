861a90097ab19b0d36bf543a0718bee30e35e9a0
Dom Charlesworth <dgc336@gmail.com>
2016-10-27 09:59:08 +0100

    Add fontlock+ for improvements to alltheicons

---
+++ packages/elpa/font-lock+-20151231.1319/font-lock+-autoloads.el
@@ -0,0 +1,15 @@
+;;; font-lock+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("font-lock+.el") (22543 16786 116429 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; font-lock+-autoloads.el ends here
---
+++ packages/elpa/font-lock+-20151231.1319/font-lock+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "font-lock+" "20151231.1319" "Enhancements to standard library `font-lock.el'." 'nil :url "http://www.emacswiki.org/font-lock+.el" :keywords '("languages" "faces" "highlighting"))
