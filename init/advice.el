4691048119e8ae4b9f5f5d6d2e04ded8b7e08447
Dom Charlesworth <dgc336@gmail.com>
2016-12-28 14:22:17 +0000

    Clean up neotree advise as icon theme is supported natively

--- init/advice.el
+++ init/advice.el
@@ -44,50 +44,7 @@
      '(markdown-header-face-2 ((t (:inherit font-lock-function-name-face :weight bold :height 1.4))))
      '(markdown-header-face-3 ((t (:inherit font-lock-function-name-face :weight bold :height 1.2)))))))
 
-(eval-after-load "neotree"
-  (defun neo-buffer--insert-fold-symbol (name &optional file-name)
-    "Custom overriding function for the fold symbol.
-`NAME' decides what fold icon to use, while `FILE-NAME' decides
-what file icon to use."
-    (or (and (equal name 'open)  (insert (all-the-icons-icon-for-dir file-name "down")))
-        (and (equal name 'close) (insert (all-the-icons-icon-for-dir file-name "right")))
-        (and (equal name 'leaf)  (insert (format "\t\t\t%s\t" (all-the-icons-icon-for-file file-name))))))
-
-  (defun neo-buffer--insert-dir-entry (node depth expanded)
-    (let ((node-short-name (neo-path--file-short-name node)))
-      (insert-char ?\s (* (- depth 1) 2)) ; indent
-      (when (memq 'char neo-vc-integration)
-        (insert-char ?\s 2))
-      (neo-buffer--insert-fold-symbol
-       (if expanded 'open 'close) node)
-      (insert-button (concat node-short-name "/")
-                     'follow-link t
-                     'face neo-dir-link-face
-                     'neo-full-path node
-                     'keymap neotree-dir-button-keymap)
-      (neo-buffer--node-list-set nil node)
-      (neo-buffer--newline-and-begin)))
-
-  (defun neo-buffer--insert-file-entry (node depth)
-    (let ((node-short-name (neo-path--file-short-name node))
-          (vc (when neo-vc-integration (neo-vc-for-node node))))
-      (insert-char ?\s (* (- depth 1) 2)) ; indent
-      (when (memq 'char neo-vc-integration)
-        (insert-char (car vc))
-        (insert-char ?\s))
-      (neo-buffer--insert-fold-symbol 'leaf node-short-name)
-      (insert-button node-short-name
-                     'follow-link t
-                     'face (if (memq 'face neo-vc-integration)
-                               (cdr vc)
-                             neo-file-link-face)
-                     'neo-full-path node
-                     'keymap neotree-file-button-keymap)
-      (neo-buffer--node-list-set nil node)
-      (neo-buffer--newline-and-begin))))
-
 (advice-add 'load-theme :before 'disable-themes)
-(advice-add 'counsel-load-theme :around 'preserve-font)
 (advice-add 'counsel-load-theme :after 'markdown-style-themes)
 (advice-add 'counsel-load-theme :after 'remove-mode-line-box)
 
