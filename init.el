05c72225d4f15a26631c5cea4f44c51fa5f299c3
Dom Charlesworth <dgc336@gmail.com>
2017-04-25 07:37:26 +0100

    [package] Add three different Lisp based highlight packages
    + highlight defined
    + highlight quoted
    + highlight parantheses

--- init.el
+++ init.el
@@ -67,6 +67,14 @@
 (use-package rainbow-mode :ensure t :defer 1
   :init (add-hook 'prog-mode-hook 'rainbow-mode))
 
+(use-package highlight-defined :ensure t :after emacs-lisp-mode
+  :init (add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode))
+(use-package highlight-quoted :ensure t :after emacs-lisp-mode
+  :init (add-hook 'emacs-lisp-mode-hook 'highlight-quoted-mode))
+(use-package highlight-parentheses :ensure t :after emacs-lisp-mode
+  :init (add-hook 'emacs-lisp-mode-hook 'highlight-parentheses-mode)
+  :config (setq hl-paren-colors '("#91ca55" "#f45d43" "#ee3f46" "#c0392b")))
+
 (use-package multiple-cursors :ensure t
   :bind ("H-n" . mc/mark-next-like-this)
         ("s-n" . mc/skip-to-next-like-this)
