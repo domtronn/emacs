68770ab75c5b024a8d1f224c5db381d3ad905d0f
Dom Charlesworth <dgc336@gmail.com>
2017-05-16 08:04:24 +0100

    [init] Replace old use package with new one

--- etc/packages/use-package-20170509.1157/use-package-autoloads.el
+++ etc/packages/use-package-20170509.1157/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22783 28290
+;;;### (autoloads nil "use-package" "use-package.el" (22810 41494
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
