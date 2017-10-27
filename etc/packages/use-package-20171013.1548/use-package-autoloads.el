0140868b6853e9420b8b5ea3c7fcfe878c4020a7
Dom Charlesworth <dgc336@gmail.com>
2017-10-27 14:03:51 +0100

    [package] ~ Update `use-package`

--- etc/packages/use-package-20171013.1548/use-package-autoloads.el
+++ etc/packages/use-package-20171013.1548/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22929 46294
+;;;### (autoloads nil "use-package" "use-package.el" (23024 22719
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
